99a4cd76dca79ac7a6a1258009a324974f8cdeea
Wen Chao Chen <chen.wenchao.private@gmail.com>
2015-10-05 23:53:02 +0200

    changing db_config

--- db/db_config.php
+++ db/db_config.php
@@ -1,5 +1,5 @@
 <?php
-	define("DB_HOST", "localhost");
+	define("DB_HOST", "http://192.168.99.100/");
 	define("DB_USER", "root");
 	define("DB_PASSWORD", "");
 	define("DB_DATABASE", "witestify");
