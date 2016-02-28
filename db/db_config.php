edf675a4bd7818a643187a5a8e0e16cf1a7dd61e
Wen Chao Chen <chen.wenchao.private@gmail.com>
2016-02-28 17:11:55 +0100

    changed config

--- db/db_config.php
+++ db/db_config.php
@@ -1,6 +1,6 @@
 <?php
-	define("DB_HOST", "http://192.168.99.100/");
+	define("DB_HOST", "http://192.168.99.100:1234");
 	define("DB_USER", "root");
-	define("DB_PASSWORD", "");
+	define("DB_PASSWORD", "1234");
 	define("DB_DATABASE", "witestify");
 ?>
